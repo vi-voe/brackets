34d4f920dcec78c925826bf7c6f86ecf37830f50
ext_vvoevodkina <vivoe@yandex.ru>
2020-10-19 15:40:13 +0300

    feat: make brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ // если нечетное количество скобок
+ if(str.length%2) return false;
+
+ //перевести строку в массив
+ let strArr = str.split('');
+ let result = false;
+
+  function test(arr){
+    arr.reduce( (prev, current, ind) => {
+      bracketsConfig.forEach(element => {
+        if(element[0] == prev && element[1] == current) {
+          arr.splice(ind-1, 2);
+          if(arr.length == 0) {
+            result = true;
+          } else {
+            test(arr)
+          }
+        }
+      });
+      return prev=current
+    })
+  }
+  test(strArr);
+
+ return result
 }
